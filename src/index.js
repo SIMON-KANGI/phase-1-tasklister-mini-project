39f593272d0c1bba08be336214e4aa4647908100
SIMON-KANGI <simonmwangikangi@gmail.com>
2023-12-27 11:46:03 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form=document.querySelector('form')
+  form.addEventListener('submit',(e)=>{
+e.preventDefault()
+handleSubmit(e.target.querySelector('#new-task-description').value)
+  })
+
+  function handleSubmit(todo){
+    let tasks=document.getElementById("tasks")
+    let list=document.createElement('li')
+    tasks.appendChild(list)
+    list.innerHTML=todo
+   
+
+
+  }
 });
